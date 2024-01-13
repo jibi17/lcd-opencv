47d025fb96c6f07b835ce7df21356be47fb81baf
heisen <houtaofly@gmali.com>
2024-01-13 23:02:56 +0800

    can play voideo

--- src/main.cpp
+++ src/main.cpp
@@ -35,16 +35,29 @@ int main(int argc, char **args)
 	LCD_Fill2(0,0,LCD_W,LCD_H,WHITE);
 	delay(100);
 
-    cv::Mat originalImage = cv::imread("./wallpaper.jpg");
-
-    if (originalImage.empty()) {
-        std::cerr << "Error loading image." << std::endl;
-        return (-1);
+    cv::VideoCapture cap("./test.mp4");
+    // 检查视频是否成功打开
+    if (!cap.isOpened()) {
+        std::cerr << "Error opening video file." << std::endl;
+        return -1;
     }
+    
+    while (true) {
+        cv::Mat frame;
+        cap >> frame;  // 从视频捕获设备读取一帧
+
+        // 检查是否成功读取一帧
+        if (frame.empty()) {
+            std::cerr << "End of video" << std::endl;
+            break;
+        }
+
+        // 在窗口中显示当前帧
+        //cv::imshow("Video", frame);
 
     // 缩放图像为320x172
     cv::Mat resizedImage;
-    cv::resize(originalImage, resizedImage, cv::Size(320, 172));
+    cv::resize(frame, resizedImage, cv::Size(320, 172));
     //cv::imshow("Resized RGB565 Image", resizedImage);
     
     //转换为RGB565格式
@@ -53,8 +66,8 @@ int main(int argc, char **args)
     // 获取图像的宽度和高度
     int width = rgb565Image.cols;
     int height = rgb565Image.rows;
-    cout<<"wideth:"<<width<<endl;
-    cout<<"height:"<<height<<endl;
+   // cout<<"wideth:"<<width<<endl;
+   // cout<<"height:"<<height<<endl;
     //int width = imageWidth;
     //int height = imageHeight;
     // 遍历图像像素并写入数组
@@ -73,7 +86,55 @@ int main(int argc, char **args)
         }
     }
 	LCD_ShowPicture2(0,0,320,172,image2);
-	delay(200);
+        // 等待一段时间，按键盘上的任意键退出循环
+        delay(10);
+    }
+
+    // 关闭窗口
+    cv::destroyWindow("Video");
+
+
+
+
+    // cv::Mat originalImage = cv::imread("./wallpaper.jpg");
+
+    // if (originalImage.empty()) {
+    //     std::cerr << "Error loading image." << std::endl;
+    //     return (-1);
+    // }
+
+    // // 缩放图像为320x172
+    // cv::Mat resizedImage;
+    // cv::resize(originalImage, resizedImage, cv::Size(320, 172));
+    // //cv::imshow("Resized RGB565 Image", resizedImage);
+    
+    // //转换为RGB565格式
+    // cv::Mat rgb565Image;
+    // cv::cvtColor(resizedImage, rgb565Image, cv::COLOR_BGR2BGR565);
+    // // 获取图像的宽度和高度
+    // int width = rgb565Image.cols;
+    // int height = rgb565Image.rows;
+    // cout<<"wideth:"<<width<<endl;
+    // cout<<"height:"<<height<<endl;
+    // //int width = imageWidth;
+    // //int height = imageHeight;
+    // // 遍历图像像素并写入数组
+    // for (int y = 0; y < height; y++) {
+    //     for (int x = 0; x < width; x++) {
+    //         // 获取RGB565值
+    //         uint16_t rgb565Value = rgb565Image.at<uint16_t>(y, x);
+
+    //         // 分开高位和低位，并写入数组
+    //         uint8_t highByte = (rgb565Value >> 8) & 0xFF;
+    //         uint8_t lowByte = rgb565Value & 0xFF;
+    //         image2[(y * width + x) * 2]=highByte;
+    //         image2[(y * width + x) * 2 + 1]=lowByte;
+    //         // outputArray << "0X" << std::hex << std::uppercase << static_cast<int>(highByte) << ",";
+    //         // outputArray << "0X" << std::hex << std::uppercase << static_cast<int>(lowByte) << ",";
+    //     }
+    // }
+	// LCD_ShowPicture2(0,0,320,172,image2);
+	// delay(200);
 	// const char* filename = "./image.h";
 
     // // 打开文件
